5095a9c869f1df9ec7f9760cf693f39ccb62e2d4
g <rhkdgh0418@daum.net>
2018-09-29 16:32:54 +0900

    학생솬리 기능추가

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class App {
-    static class Members{
+    static class Member{
         protected String name;
         protected String email;
         protected String password;
@@ -23,47 +23,136 @@ public class App {
         public void setPassword(String password) {
             this.password = password;
         }
+    }
+    
+    static class Student extends Member{
+        protected String school;
+        protected boolean working;
+        protected String tel;
+        public String getSchool() {
+            return school;
+        }
+        public void setSchool(String school) {
+            this.school = school;
+        }
+        public boolean isWorking() {
+            return working;
+        }
+        public void setWorking(boolean working) {
+            this.working = working;
+        }
+        public String getTel() {
+            return tel;
+        }
+        public void setTel(String tel) {
+            this.tel = tel;
+        }
         
         
     }
     
-    static Members[] members=new Members[100];  
+    static Student[] students = new Student[100];
+
     static int index=0;
-    static Scanner sc=new Scanner(System.in);
-       public static void main(String[] args) {
-     inputmember();
-     ptrintmember();
-       
-       sc.close();
-     
-       }
-       static void inputmember() {
-           while(true) {
-               Members m=new Members();
-               System.out.println("아이디");
-               m.setName(sc.nextLine());
-               System.out.println("이메일");
-               m.setEmail(sc.nextLine());
-               System.out.println("비밀번호");
-               m.setPassword(sc.nextLine());
-               
-               
-               members[index++]=m;
-               
-               System.out.println("계속 입력하시겠습니까??");
-               String yn=sc.nextLine();
-               if(yn.toUpperCase().equals("N"))
-                   break;
-               
-               }
-           
-       }
-       static void ptrintmember() {
-           for(int i=0;i<index;i++) {
-               System.out.printf("%s,%s,%s\n"
-                       ,members[i].getName(),
-                       members[i].getEmail(),
-                       members[i].getPassword());
-               }
-       }
-}
+    static Scanner keyIn=new Scanner(System.in);
+    public static void main(String[] args) {
+
+        while(true) {
+           String menu = promtMenu();
+
+           if(menu.equals("1")) {
+               serviceStduentMenu();
+           } else if(menu.equals("0")) {
+               System.out.println("안녕히 가세요");
+               break;
+           }
+        }
+
+
+
+        keyIn.close();
+
+    }
+
+    private static void serviceStduentMenu() {
+        while(true) {
+            System.out.println("학생 관리> ");
+            String command = keyIn.nextLine();
+            if(command.equals("list")){
+                ptrintStudents();
+            } else if (command.equals("add")) {
+                inputStudents();
+            } else if (command.equals("quit")) {
+                break;
+            } else {
+                System.out.println("유효하지 않는 명령입니다.");
+            }
+        } 
+    }
+
+    private static String promtMenu() {
+        System.out.println("[메뉴]");
+        System.out.println("1.학생 관리");
+        System.out.println("2.강사 관리");
+        System.out.println("3.매니저 관리");
+        System.out.println("0.종료 관리");
+
+        while(true) {
+            System.out.println("메뉴 번호");
+
+            String menu =keyIn.nextLine();
+
+            switch (menu){
+            case "1":
+            case "2":
+            case "3":
+            case "0":
+                return menu;
+            default:
+                System.out.println("메뉴 번호가 유효하지 않습니다.");
+            }
+        }
+    }
+    static void inputStudents() {
+        while(true) {
+            Student m=new Student();
+            System.out.println("아이디");
+            m.setName(keyIn.nextLine());
+            System.out.println("이메일");
+            m.setEmail(keyIn.nextLine());
+            System.out.println("비밀번호");
+            m.setPassword(keyIn.nextLine());
+            System.out.println("최종학력?");
+            m.setSchool(keyIn.nextLine());
+            System.out.println("재직여부?(true/false)");
+            m.setWorking(Boolean.parseBoolean(keyIn.nextLine()));
+            System.out.println("전화");
+            m.setTel(keyIn.nextLine());
+
+
+            students[index++]=m;
+
+            System.out.println("계속 입력하시겠습니까??");
+            String yn=keyIn.nextLine();
+            if(yn.toUpperCase().equals("N"))
+                break;
+
+        }
+
+    }
+    static void ptrintStudents() {
+        int count =0;
+        for(Student s: students) {
+            if(count++ == index)
+                break;
+        System.out.printf("%s, %s, %s, %s, %b, %s\n",
+                s.getName(),
+                s.getEmail(),
+                s.getPassword(),
+                s.getSchool(),
+                s.isWorking(),
+                s.getTel());
+        }
+    }
+    }
+
