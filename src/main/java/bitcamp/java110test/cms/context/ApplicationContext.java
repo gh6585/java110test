f1724ba1eea53afacb5b2fb5e7edd394cf977a3d
g <rhkdgh0418@daum.net>
2018-10-05 21:09:59 +0900

    injectDependency()를 별도 클래스로 분리

--- src/main/java/bitcamp/java110test/cms/context/ApplicationContext.java
+++ src/main/java/bitcamp/java110test/cms/context/ApplicationContext.java
@@ -30,20 +30,22 @@ public class ApplicationContext {
 
         // 패키지 폴더에 들어 있는 클래스를 찾아 인스턴스를 생성하여 objPool에 보관한다.
         findClass(file, path);
-        
+
         // 로딩된 클래스 목록을 뒤져서 @Commponent 가 붙은
         // 클래스에 대해 인스턴스를 생성하여 objPool에 보관한다.
         createInstance();
-        
-        //objPool에 보관된 객체를 꺼내 @Autowired가 붙은 셋터를 찾아 호출한다.
-        // => 의존 객체 주입
-        injectDependency();
+
+        // injectDependency() 메서드를 외부 클래스로 분리한 다음에
+        // 그 객체를 실행한다.
+        AutowiredAnnoationBeanPostProcessor processor =
+                new AutowiredAnnoationBeanPostProcessor();
+        processor.postProcess(this);
     }
     // objPool에 보관된 객체를 이름으로 찾아 리턴한다.
     public Object getBean(String name) {
         return objPool.get(name);
     }
-    
+
     public Object getBean(Class<?> type) {
         Collection<Object> objList = objPool.values();
         for(Object obj : objList) {
@@ -72,33 +74,33 @@ public class ApplicationContext {
                 try {
                     // 클래스 이름을 가지고 .class 파일을 찾아 메모리에 로딩한다.
                     Class<?> clazz = Class.forName(className);
-                    
+
                     classes.add(clazz);
-                    
+
                 } catch (Exception e) {}
             }
         }
     }
-    
+
     private void createInstance() {
         for(Class<?> clazz : classes) {
             // => 인터페이스인 경우 무시한다.
             if(clazz.isInterface()) continue;
-            
+
             // => 클래스에서 Component 애노테이션을 추출한다.
             Component anno = clazz.getAnnotation(Component.class);
-            
+
             // => @Component 애노테이션이 붙지 않은 클래스는 객체르 생성하지 않는다.
             if(anno == null) continue;
-            
+
             try {
                 // 로딩된 클래스정보를 가지고 인스턴스를 생성한다.
                 // => 먼저 해당 클래스의 생성자 정보를 얻는다.
                 Constructor<?> constructor = clazz.getConstructor();
-                
+
                 // => 생성자를 가지고 인스턴스를 생성한다.
                 Object instance = constructor.newInstance();
-                
+
                 // => Component 애노테이션이 value 값이 있으면 그 값으로 객체를 저장
                 //    없으면 , 클래스 이름으로 객체를 저장한다.
                 if(anno.value().length()>0) {
@@ -110,34 +112,9 @@ public class ApplicationContext {
             }catch(Exception e) {
                 e.printStackTrace();
                 System.out.println("%s 클래스는 기본 생성자가 없습니다.");
-                        clazz.getName();
+                clazz.getName();
             }
         }
     }
-    
-    private void injectDependency() {
-        // objPool에 보관된 객체 목록을 꺼낸다.
-        Collection<Object> objList = objPool.values();
-        
-        for(Object obj : objList) {
-            // 목록에서 객체를 꺼내 @Autowired가 붙은 메서드를 찾는다.
-            Method[] methods = obj.getClass().getDeclaredMethods();
-            for(Method m : methods) {
-                if(!m.isAnnotationPresent(Autowired.class)) continue;
-                
-                // setter 메서드의 파라미터 타입을 알아낸다.
-                Class<?> paramType = m.getParameterTypes()[0];
-                
-                // 그 파라미터 타입과 일치하는 개체가 objPool에서 꺼낸다.
-                Object dependency = getBean(paramType);
-                
-                if(dependency == null) continue;
-                
-                try {
-                    m.invoke(obj, dependency);
-                    System.out.printf("%s() 호출됨\n", m.getName());
-                }catch(Exception e) {}
-            }
-        }
-    }
+
 }
