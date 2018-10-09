eb5f1cc0bf11e4389fca55779ad62605535281bd
g <rhkdgh0418@daum.net>
2018-10-09 14:18:36 +0900

    ,

--- src/main/java/bitcamp/java110/cms/dao/ManagerDao.java
+++ src/main/java/bitcamp/java110/cms/dao/ManagerDao.java
@@ -6,8 +6,10 @@ import bitcamp.java110.cms.domain.Manager;
 
 public interface ManagerDao {
     int insert(Manager manager)
-    throws MandatoryValueDaoException,DuplicationDaoException;
+            throws MandatoryValueDaoException,DuplicationDaoException;
     List<Manager> findAll();
     Manager findByEmail(String email);
-    int delete(String email);
+    default Manager findByNo(int no) {return null;}
+    default int delete(String email) {return 0;}
+    default int deleteByNo(int no) {return 0;}
 }
